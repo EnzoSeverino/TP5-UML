68669acbb510fc114a745c555e0ca65451426a93
EnzoSeverino <severinoenzo0@gmail.com>
2025-05-26 19:09:32 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+
+    private int id;
+    private String nombre;
+    private String email;
+
+    public Usuario(int id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
